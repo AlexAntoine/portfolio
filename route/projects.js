af3859614034d0f443ef9eb951e090240002c69a
Alex Antoine <antoinealex1234@gmail.com>
2022-12-12 16:21:32 -0500

    added route to upload image content

--- route/projects.js
+++ route/projects.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getAllProjects, getSingleProject, createProject, uploadPhotos, updateProject, deleteProject} = require('../controller/projects')
+const {getAllProjects, getSingleProject, createProject, uploadPhotos, updateProject, deleteProject, uploadContent} = require('../controller/projects')
 
 router.route('/').get(getAllProjects).post(createProject);
 
 router.route('/:id').get(getSingleProject).put(updateProject).delete(deleteProject).post(uploadPhotos);
+
+router.route('/:id/content').post(uploadContent)
 
 
 module.exports = router;
