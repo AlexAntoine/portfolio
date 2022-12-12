1adef8ec864b865bf63ea13907f3347527dad2c4
AlexAntoine <anotoinealex1234@gmail.com>
2022-12-11 23:31:09 -0500

    added CRUD functionality for projects

--- controller/projects.js
+++ controller/projects.js
@@ -9,8 +9,42 @@ const data= {
 // @route Get /api/v1/projects
 // @Access Public
 exports.getAllProjects = asyncHandler(async(req, res, next)=>{
-    console.log('hello')
-    const result = await Projects.findOne();
+    const result = await Projects.find();
 
-    console.log('line 15: ',result);
-})
+    res.status(200).json({success: true, data:result})
+});
+
+// @desc Get single Project
+// @route Get /api/v1/projects/:projects
+// @Access Public
+exports.getSingleProject = asyncHandler(async(req, res, next)=>{
+
+    const project = await Projects.findOne({_id:req.params.id})
+
+    res.status(200).json({success: true, data:project});
+});
+
+// @desc Update Project
+// @route PUT /api/v1/projects/:projects
+// @Access Public
+exports.updateProject = asyncHandler(async(req, res, next)=>{
+    
+   const updateProject = await Projects.findByIdAndUpdate(req.params.id, req.body,{
+        new:true,
+        runValidators: true
+    });
+    console.log('line 36: ', updateProject);
+    res.status(200).json({success: true, data:updateProject});
+});
+
+// @desc Delete Project
+// @route Delete /api/v1/projects/:projects
+// @Access Public
+exports.deleteProject = asyncHandler(async(req, res, next)=>{
+
+    const project = await Projects.findById(req.params.id);
+
+    await project.remove();
+
+    res.status(200).json({success: true, data:{}});
+});
