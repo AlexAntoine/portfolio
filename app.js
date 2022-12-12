d7b7f1e92731ce4f978e04ce5f35bb6106c3d129
AlexAntoine <anotoinealex1234@gmail.com>
2022-12-11 23:20:03 -0500

    adde middleware

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 require('dotenv').config({path:'./config/.env'});
+const mongoose = require('mongoose');
 const express = require('express');
 const {localDB} = require('./db/mongoose');
 const colors = require('colors');
 const path = require('path');
 
+mongoose.set('strictQuery', true)
+
 const app = express();
 app.use(express.json());
+
+localDB();
 
 //Routes
 const projectsRouter = require('./route/projects');
