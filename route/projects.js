28baccc3cb38697a9b3ed647949378bc3801bc73
AlexAntoine <anotoinealex1234@gmail.com>
2022-12-11 23:20:55 -0500

    added new routes

--- route/projects.js
+++ route/projects.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getAllProjects} = require('../controller/projects')
+const {getAllProjects, getSingleProject, updateProject, deleteProject} = require('../controller/projects')
 
 router.route('/').get(getAllProjects);
+
+router.route('/:id').get(getSingleProject).put(updateProject).delete(deleteProject);
 
 
 module.exports = router;
