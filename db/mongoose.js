038437263d330c0140ac5aaaa601cc25a00440c3
AlexAntoine <anotoinealex1234@gmail.com>
2022-12-11 23:20:47 -0500

    change connection string

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const localDB = async()=>{
-    return await mongoose.connect('mongodb://localhost:27017/project-api'.cyan.blue,{useNewUrlParser:true, useUnifiedTopology: true })
+    return await mongoose.connect('mongodb://127.0.0.1/project-api',{useNewUrlParser:true, useUnifiedTopology: true })
 }
 
 const prodDB = async()=>{
